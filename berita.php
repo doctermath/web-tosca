01d3200a9516616edd06c067714617b922ef2ae8
MedioseAlfranda <63895661+MedioseAlfranda@users.noreply.github.com>
2020-04-18 21:20:56 +0700

    Update berita.php

--- berita.php
+++ berita.php
@@ -4,11 +4,40 @@
   </head>
   <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+  <!-- Bootstrap -->
+		<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css">
+
+		<!-- Owl Carousel -->
+		<link type="text/css" rel="stylesheet" href="css/owl.carousel.css" >
+		<link type="text/css" rel="stylesheet" href="css/owl.theme.default.css" >
+		
+		<!-- Font Awesome Icon -->
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+
+		<!-- Custom stlylesheet -->
+		<link type="text/css" rel="stylesheet" href="css/style.css"/>
+
+
   <style>
 
 .body-container{
 	width:70%;
 	margin: 0px auto;
+	padding-top:50px ;
+    transition: all 1.5s ease;
+}
+
+.body-container1{
+	width:70%;
+	margin: 0px auto;
+	padding-top:50px ;
+    transition: all 1.5s ease;
+}
+
+.body-container2{
+	width:70%;
+	margin: 0px auto;
+	padding-top:50px ;
     transition: all 1.5s ease;
 }
 
@@ -23,7 +52,7 @@
 }
 
 @media only screen and (max-width:500px){
-.div{
+.card-group{
 	width:100%;
     transition: all 1.5s ease-in-out;
 	clear:both;
@@ -37,45 +66,129 @@
   <body>
 
 
-  <div class = "container">
-    <div class="div">
-      	<img src="1.jpg" style="width:100%">
-      	<div class="card-footer">
-      <small class="text-muted"><strong><p align ="center">Fastek Fair 2019</p></strong></small>
-                <p> Salah Satu acara yang ga kalah serunya di UKMC...</p>
-                <p align="center"><a href="#" class="w3-card">Selengkapnya</a></p>
-      <div class="card-footer">
-      <small class="text-muted">diposting 3 menit lalu</small>
-        </div>
-    </div>
- </div>
-</div>
- <div class = "container">
- <div class="div">
-      	<img src="2.png" style="width:100%">
-      	<div class="card-footer">
-      <small class="text-muted"><strong><p align ="center"> 3 Mahasiswa Menjuarai Kompetisi Hackathon</p></strong></small>
-                <p> Suatu Kebanggan ketika 3 orang Mahasiswa memenangkan Kompetisi besar..</p>
-                <p align="center"><a href="#" class="w3-card">Selengkapnya</a></p>
-      <div class="card-footer">
-      <small class="text-muted">diposting 3 menit lalu</small>
-        </div>
-    </div>
- </div>
+<div class="body-container">
+<p align ="justify" > <h1>Berita dan pengumuman</h1></p>
 </div>
 
-<div class = "container">
- <div class="div">
-      	<img src="2.png" style="width:100%">
-      	<div class="card-footer">
-      <small class="text-muted"><strong><p align ="center"> Musi Management 2020</p></strong></small>
-                <p> Musi Management Student Club (MMSC) kembali menggelar lomba presentasi tingkat nasional, Musi Charitas Management Celebration (MCMC), yang berlangsung pada 5 – 7 November 2019.</p>
-                <p align="center"><a href="#" class="w3-card">Selengkapnya</a></p>
-      <div class="card-footer">
-      <small class="text-muted">diposting 3 menit lalu</small>
-        </div>
+<div class="body-container">
+<div class="card-group">
+  <div class="card">
+    <img class="card-img-top" src="1.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">FASTEK FAIR 2020</h5>
+      <strong><p class="card-text">Fastek Fair merupakan kegiatan tahunan yang diselenggarakan oleh organisasi kemahasiswaan Fakultas Sains dan Teknologi Universitas Katolik Musi Charitas. </p></strong>
     </div>
- </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+
+  <div class="card">
+    <img class="card-img-top" src="2.png" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">HACKATHON 2020</h5>
+     <strong> <p class="card-text">Sebuah pekan retas atau hackathon atau hackfest, atau codefest adalah sebuah acara kolaborasi pengembangan proyek perangkat lunak</p></strong>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+
+  <div class="card">
+    <img class="card-img-top" src="3.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">FIT COMPETITION 2020</h5>
+    <strong> <p class="card-text">FIT COMPETITION 2020 adalah salah satu kompetisi/lomba yang telah dibuka bagi para programing ataupun yang lainnya. Dalam lomba ini terdapat tiga kategori yang dilombakan...</p></strong>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+
+
+</div>
+</div>
+
+
+<div class="body-container1">
+<div class="card-group">
+  <div class="card">
+    <img class="card-img-top" src="1.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">FASTEK FAIR 2020</h5>
+      <strong><p class="card-text">Fastek Fair merupakan kegiatan tahunan yang diselenggarakan oleh organisasi kemahasiswaan Fakultas Sains dan Teknologi Universitas Katolik Musi Charitas. </p></strong>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+
+  <div class="card">
+    <img class="card-img-top" src="2.png" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">HACKATHON 2020</h5>
+     <strong> <p class="card-text">Sebuah pekan retas atau hackathon atau hackfest, atau codefest adalah sebuah acara kolaborasi pengembangan proyek perangkat lunak</p></strong>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+
+  <div class="card">
+    <img class="card-img-top" src="3.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">FIT COMPETITION 2020</h5>
+    <strong> <p class="card-text">FIT COMPETITION 2020 adalah salah satu kompetisi/lomba yang telah dibuka bagi para programing ataupun yang lainnya. Dalam lomba ini terdapat tiga kategori yang dilombakan...</p></strong>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+
+
+</div>
+</div>
+
+
+<div class="body-container1">
+<div class="card-group">
+  <div class="card">
+    <img class="card-img-top" src="1.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">FASTEK FAIR 2020</h5>
+      <strong><p class="card-text">Fastek Fair merupakan kegiatan tahunan yang diselenggarakan oleh organisasi kemahasiswaan Fakultas Sains dan Teknologi Universitas Katolik Musi Charitas. </p></strong>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+
+  <div class="card">
+    <img class="card-img-top" src="2.png" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">HACKATHON 2020</h5>
+     <strong> <p class="card-text">Sebuah pekan retas atau hackathon atau hackfest, atau codefest adalah sebuah acara kolaborasi pengembangan proyek perangkat lunak</p></strong>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+
+  <div class="card">
+    <img class="card-img-top" src="3.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">FIT COMPETITION 2020</h5>
+    <strong> <p class="card-text">FIT COMPETITION 2020 adalah salah satu kompetisi/lomba yang telah dibuka bagi para programing ataupun yang lainnya. Dalam lomba ini terdapat tiga kategori yang dilombakan...</p></strong>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+
+</div>
+</div>
+
+     </div>
 </div>
 
 
