f5c914c4c645e8aead4f9f02cdd9592bfb34307e
MedioseAlfranda <63895661+MedioseAlfranda@users.noreply.github.com>
2020-04-18 15:16:51 +0700

    Create berita.php

---
+++ berita.php
@@ -0,0 +1,90 @@
+<html>
+  <head>
+  <title>Berita dan Pengumuman</title>
+  </head>
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+  <style>
+
+.body-container{
+	width:70%;
+	margin: 0px auto;
+    transition: all 1.5s ease;
+}
+
+.div{
+	width:33.33%;
+	border:2px solid #ccc;
+	border-radius:12px;
+	float:left;
+	padding:5px;
+	box-sizing: border-box;
+	transition: all 1.5s ease;
+}
+
+@media only screen and (max-width:500px){
+.div{
+	width:100%;
+    transition: all 1.5s ease-in-out;
+	clear:both;
+
+	padding:10px  10px;
+}
+	
+}
+
+  </style>
+  <body>
+
+
+  <div class = "container">
+    <div class="div">
+      	<img src="1.jpg" style="width:100%">
+      	<div class="card-footer">
+      <small class="text-muted"><strong><p align ="center">Fastek Fair 2019</p></strong></small>
+                <p> Salah Satu acara yang ga kalah serunya di UKMC...</p>
+                <p align="center"><a href="#" class="w3-card">Selengkapnya</a></p>
+      <div class="card-footer">
+      <small class="text-muted">diposting 3 menit lalu</small>
+        </div>
+    </div>
+ </div>
+</div>
+ <div class = "container">
+ <div class="div">
+      	<img src="2.png" style="width:100%">
+      	<div class="card-footer">
+      <small class="text-muted"><strong><p align ="center"> 3 Mahasiswa Menjuarai Kompetisi Hackathon</p></strong></small>
+                <p> Suatu Kebanggan ketika 3 orang Mahasiswa memenangkan Kompetisi besar..</p>
+                <p align="center"><a href="#" class="w3-card">Selengkapnya</a></p>
+      <div class="card-footer">
+      <small class="text-muted">diposting 3 menit lalu</small>
+        </div>
+    </div>
+ </div>
+</div>
+
+<div class = "container">
+ <div class="div">
+      	<img src="2.png" style="width:100%">
+      	<div class="card-footer">
+      <small class="text-muted"><strong><p align ="center"> Musi Management 2020</p></strong></small>
+                <p> Musi Management Student Club (MMSC) kembali menggelar lomba presentasi tingkat nasional, Musi Charitas Management Celebration (MCMC), yang berlangsung pada 5 – 7 November 2019.</p>
+                <p align="center"><a href="#" class="w3-card">Selengkapnya</a></p>
+      <div class="card-footer">
+      <small class="text-muted">diposting 3 menit lalu</small>
+        </div>
+    </div>
+ </div>
+</div>
+
+
+
+
+
+
+
+
+  </div>
+  </body>
+  </html>
