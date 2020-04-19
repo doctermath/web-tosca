a3356664245814652906f080b60c77cdd130b6a0
MedioseAlfranda <63895661+MedioseAlfranda@users.noreply.github.com>
2020-04-19 15:20:18 +0700

    commit berita 1

---
+++ berita1.php
@@ -0,0 +1,203 @@
+<html>
+  <head>
+  <title>Berita dan Pengumuman</title>
+  </head>
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+  <!-- Bootstrap -->
+		<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css">
+
+		<!-- Owl Carousel -->
+		<link type="text/css" rel="stylesheet" href="css/owl.carousel.css" >
+		<link type="text/css" rel="stylesheet" href="css/owl.theme.default.css" >
+		
+		<!-- Font Awesome Icon -->
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+
+		<!-- Custom stlylesheet -->
+		<link type="text/css" rel="stylesheet" href="css/style.css"/>
+
+
+  <style>
+
+.body-container{
+	width:70%;
+	margin: 0px auto;
+	padding-top:50px ;
+    transition: all 1.5s ease;
+}
+
+.body-container1{
+	width:70%;
+	margin: 0px auto;
+	padding-top:50px ;
+    transition: all 1.5s ease;
+}
+
+.body-container2{
+	width:70%;
+	margin: 0px auto;
+	padding-top:50px ;
+    transition: all 1.5s ease;
+}
+
+.div{
+	width:33.33%;
+	border:2px solid #ccc;
+	border-radius:12px;
+	float:left;
+	padding:5px;
+	box-sizing: border-box;
+	transition: all 1.5s ease;
+}
+
+@media only screen and (max-width:500px){
+.card-group{
+	width:100%;
+    transition: all 1.5s ease-in-out;
+	clear:both;
+
+	padding:10px  10px;
+}
+	
+}
+
+  </style>
+  <body>
+
+
+<div class="body-container">
+<p align ="justify" > <h1>Berita dan pengumuman</h1></p>
+</div>
+
+<div class="body-container">
+<div class="card-group">
+  <div class="card">
+    <img class="card-img-top" src="1.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">FASTEK FAIR 2020</h5>
+      <strong><p class="card-text">Fastek Fair merupakan kegiatan tahunan yang diselenggarakan oleh organisasi kemahasiswaan Fakultas Sains dan Teknologi Universitas Katolik Musi Charitas. </p></strong>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+
+  <div class="card">
+    <img class="card-img-top" src="2.png" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">HACKATHON 2020</h5>
+     <strong> <p class="card-text">Sebuah pekan retas atau hackathon atau hackfest, atau codefest adalah sebuah acara kolaborasi pengembangan proyek perangkat lunak</p></strong>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+
+  <div class="card">
+    <img class="card-img-top" src="3.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">FIT COMPETITION 2020</h5>
+    <strong> <p class="card-text">FIT COMPETITION 2020 adalah salah satu kompetisi/lomba yang telah dibuka bagi para programing ataupun yang lainnya. Dalam lomba ini terdapat tiga kategori yang dilombakan...</p></strong>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+
+
+</div>
+</div>
+
+
+<div class="body-container1">
+<div class="card-group">
+  <div class="card">
+    <img class="card-img-top" src="1.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">FASTEK FAIR 2020</h5>
+      <strong><p class="card-text">Fastek Fair merupakan kegiatan tahunan yang diselenggarakan oleh organisasi kemahasiswaan Fakultas Sains dan Teknologi Universitas Katolik Musi Charitas. </p></strong>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+
+  <div class="card">
+    <img class="card-img-top" src="2.png" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">HACKATHON 2020</h5>
+     <strong> <p class="card-text">Sebuah pekan retas atau hackathon atau hackfest, atau codefest adalah sebuah acara kolaborasi pengembangan proyek perangkat lunak</p></strong>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+
+  <div class="card">
+    <img class="card-img-top" src="3.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">FIT COMPETITION 2020</h5>
+    <strong> <p class="card-text">FIT COMPETITION 2020 adalah salah satu kompetisi/lomba yang telah dibuka bagi para programing ataupun yang lainnya. Dalam lomba ini terdapat tiga kategori yang dilombakan...</p></strong>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+
+
+</div>
+</div>
+
+
+<div class="body-container1">
+<div class="card-group">
+  <div class="card">
+    <img class="card-img-top" src="1.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">FASTEK FAIR 2020</h5>
+      <strong><p class="card-text">Fastek Fair merupakan kegiatan tahunan yang diselenggarakan oleh organisasi kemahasiswaan Fakultas Sains dan Teknologi Universitas Katolik Musi Charitas. </p></strong>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+
+  <div class="card">
+    <img class="card-img-top" src="2.png" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">HACKATHON 2020</h5>
+     <strong> <p class="card-text">Sebuah pekan retas atau hackathon atau hackfest, atau codefest adalah sebuah acara kolaborasi pengembangan proyek perangkat lunak</p></strong>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+
+  <div class="card">
+    <img class="card-img-top" src="3.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">FIT COMPETITION 2020</h5>
+    <strong> <p class="card-text">FIT COMPETITION 2020 adalah salah satu kompetisi/lomba yang telah dibuka bagi para programing ataupun yang lainnya. Dalam lomba ini terdapat tiga kategori yang dilombakan...</p></strong>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+
+</div>
+</div>
+
+     </div>
+</div>
+
+
+
+
+
+
+
+
+  </div>
+  </body>
+  </html>
