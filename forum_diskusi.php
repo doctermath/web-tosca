404e725a35afe41721e2b5597db9aea1121d0588
bobbi setiawan <bobbisetiawan@gmail.com>
2020-04-19 09:38:29 +0700

    masukan 1 real
    sudah pasti

---
+++ forum_diskusi.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+		<title> Forum Diskusi </title>
+		
+		<meta charset="utf-8">
+		<meta name=”viewport” content=”width=device-width, initial-scale=1″>
+
+		<link rel='stylesheet' type='text/css' href='b-bootstrap4/css/bootstrap.css'/>
+		<link rel='stylesheet' type='text/css' href='b-bootstrap4/css/bootstrap-grid.css'/>
+		
+		
+	</head>
+
+	<body> <!-- dibagi menjadi 2 bagian yaitu sisi kiri dan sisi kanan -->
+			<div class='row'> 
+			
+				<!-- start bagian kiri -->
+				<div class='col-6' style='padding-top: 10px;'>
+				
+					<div class='btn-primary' style='display: inline-block; padding: 5px 38px 5px 0px; border-radius: 0px 50% 50% 0px; -webkit-text-stroke: 0.3px black; background: linear-gradient(to bottom right, #ff5050 14%, #800000 28%);'>
+							<h1> Forum Komunikasi </h1>
+					</div>
+					
+					<br/> <br/> <!-- supaya tidak berdempetan -->
+					
+					<div class='btn-primary' style='display: inline-block; padding: 7px 20px 5px 0px; border-radius: 0px 50% 50% 0px; -webkit-text-stroke: 0.1px black; background: linear-gradient(to bottom right, #ff5050 14%, #800000 28%);'>
+						<h3> Pertanyaan </h3>
+					</div>				
+					
+				</div> <!-- stop bagian kiri -->
+				
+				<!-- start bagian kanan -->
+				<div class='col-6'>
+					<h1  style='padding-top: 40px;'> </h1>
+					
+					<div style='padding-left: 70px;'>
+						<div class='btn' title='pertanyaan baru'>
+							<div class='btn-secondary' style='border-radius: 5%;'> 
+								<div class='btn-lg' style='border-radius: 5%; background: linear-gradient(to bottom right, #ff5050 14%, #800000 28%);'>
+									<a href='form_buat_pertanyaan.html' target='_blank' style='color: white;'> 
+										Buat Pertanyaan 
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div> <!-- stop bagian kanan -->
+				
+			</div> <!-- akhir dari pembagian 2 sisi -->
+
+			<div class='container'>
+					<div class='shadow-sm' style='color: gray;'>
+						<div class='border' style='padding: 20px; color: black;'>
+							Pertanyaan Lorem Ipsum. "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..." "Tidak ada yang menyukai kepedihan, yang ...
+							Lorem Ipsum. "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..." "Tidak ada yang menyukai kepedihan, yang ...
+							Lorem Ipsum. "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..." "Tidak ada yang menyukai kepedihan, yang ...
+							Lorem Ipsum. "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..." "Tidak ada yang menyukai kepedihan, yang ...
+						</div>
+					</div>
+					
+					<div class='shadow-sm' style='color: gray;'>
+						<div style='padding-top: 10px;'>
+							<div class='border' style='padding: 20px; color: black;'>
+								Jawaban Lorem Ipsum. "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..." "Tidak ada yang menyukai kepedihan, yang ...
+								Lorem Ipsum. "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..." "Tidak ada yang menyukai kepedihan, yang ...
+								Pertanyaan Lorem Ipsum. "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..." "Tidak ada yang menyukai kepedihan, yang ... 
+								Lorem Ipsum. "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..." "Tidak ada yang menyukai kepedihan, yang ... 
+								Lorem Ipsum. "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..." "Tidak ada yang menyukai kepedihan, yang ... 
+								Lorem Ipsum. "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..." "Tidak ada yang menyukai kepedihan, yang ...
+							</div>
+						</div>
+					</div>
+					
+					<form action='' method='get'>
+						
+						<div class='shadow-sm' style='color: gray;'>
+							<div style='padding-right: 0px; padding-top: 10px;'>
+								<input type='text' name='komentar' placeholder='Masukkan Komentar' required style='width: 100%'/>
+							</div>
+						</div>
+						
+						<div style='padding-right: 20px; padding-top: 10px;'>
+							<input type='submit' value='Kirim' style='float: right; background: linear-gradient(to bottom right, #ff5050 14%, #800000 28%); color: white;'/>
+						</div>
+						
+					</form>
+			</div>
+	</body>
+
+</html>
