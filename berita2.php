60554470e39aad1a6206496c0655560fd1835b32
MedioseAlfranda <63895661+MedioseAlfranda@users.noreply.github.com>
2020-04-19 21:37:58 +0700

    Terbaru-Commit Second
    Ini yg lah sudah selesai yordan

---
+++ berita2.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Berita dan Pengumuman</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/blog-home.css" rel="stylesheet">  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+  <!-- Bootstrap -->
+    <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- Owl Carousel -->
+    <link type="text/css" rel="stylesheet" href="css/owl.carousel.css" >
+    <link type="text/css" rel="stylesheet" href="css/owl.theme.default.css" >
+    
+    <!-- Font Awesome Icon -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+
+    <!-- Custom stlylesheet -->
+    <link type="text/css" rel="stylesheet" href="css/style.css"/>
+
+
+</head>
+
+<body>
+
+ 
+  <!-- Page Content -->
+  <div class="container">
+
+    <div class="row">
+
+      <!-- Blog Entries Column -->
+      <div class="col-md-8">
+
+        <a href="berita.php" class="my-4"> 
+          <h1>Berita dan Pengumuman</h1>
+        </a>
+
+        <!-- Blog Post -->
+        <div class="card mb-4">
+          <img class="card-img-top" src="2.png" alt="Card image cap">
+          <div class="card-body">
+            <h2 class="card-title">Hackathon 2020</h2>
+            <strong>
+            <p align="justify" class="card-text">kompetisi Hackathon perdana berskala nasional dengan mengusung tema "Hack of Thrones: Beyond Artificial Intelliigence In Collaboration with Bank Bukopin."</p>
+
+            <p align="justify" class="card-text">Registrasi Online kompetisi ini dimulai sejak 3 Februari 2020 hingga 17 Maret 2020. Peserta yang telah melakukan registrasi selanjutnya akan diseleksi dan 20 tim terpilih dalam Final Hackathon akan diumumkan pada 20 Maret 2020.</p>
+
+           
+            </strong>
+
+
+          </div>
+          <div class="card-footer text-muted">
+          </div>
+        </div>
+
+        
+       
+
+        <!-- Pagination -->
+        <ul class="pagination justify-content-center mb-4">
+          <li class="page-item">
+            <a class="page-link" href="#">&larr; Older</a>
+          </li>
+          <li class="page-item disabled">
+            <a class="page-link" href="#">Newer &rarr;</a>
+          </li>
+        </ul>
+
+      </div>
+
+      <!-- Sidebar Widgets Column -->
+      <div class="col-md-4">
+
+        <!-- Search Widget -->
+        <div class="card my-4">
+          <h5 class="card-header">Search</h5>
+          <div class="card-body">
+            <div class="input-group">
+              <input type="text" class="form-control" placeholder="Search for...">
+              <span class="input-group-btn">
+                <button class="btn btn-secondary" type="button">Go!</button>
+              </span>
+            </div>
+          </div>
+        </div>
+
+        
+
+        <!-- Side Widget -->
+        <div class="card my-4">
+          <h5 class="card-header" style="background-color:#FF3030; color:white;">Berita Terkini</h5>
+          <div class="card-body">
+            <a href="berita1.php">Fastek Fair 2020</a>
+                            <br>------------------------------------------</br>
+              <a href="berita2.php">Hackathon 2020</a>
+                            <br>------------------------------------------</br>
+             <a href="berita.php">Fit Competition 2020</a>
+                          
+           
+
+             <div class="card my-4">
+          <h5 class="card-header" style="background-color:#FF3030; color:white;">Pengumuman terkini</h5>
+          <div class="card-body">
+             <a href="berita1.php">Pendaftaran Calon Mahasiswa UKMC</a>
+                            <br>------------------------------------------</br>
+             <a href="berita.php">PENDAFTARAN WISUDA X GENAP 2019-2020</a>
+                            <br>------------------------------------------</br>
+             <a href="berita.php">Kuliah Online Selama Pandemi Corona</a>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+    <!-- /.row -->
+
+  </div>
+  <!-- /.container -->
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
